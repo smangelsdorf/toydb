a18bdc210f86a41fbfb03b3fa4582f9ba3101c1b
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2020-12-18 15:10:20 +1000

    Add test for CLI output, and fix bug

--- src/cli.rs
+++ src/cli.rs
@@ -17,3 +17,22 @@ struct Context<'a> {
     buf: &'a mut String,
     table: &'a mut Table,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::r#loop::cli_loop;
+
+    #[test]
+    fn cli_interaction_test() {
+        let input: &'_ [u8] = b"insert 1 abc test@example.com\nselect\n\\q\n";
+        let mut output: Vec<u8> = Vec::with_capacity(256);
+
+        cli_loop(input, &mut output).unwrap();
+
+        let output_string = String::from_utf8(output).unwrap();
+        assert_eq!(
+            "> Success.\n> (1, abc, test@example.com)\n1 rows.\n> Bye!\n",
+            output_string
+        );
+    }
+}
--- src/cli/statement.rs
+++ src/cli/statement.rs
@@ -14,11 +14,13 @@ pub(in crate::cli) fn handle_statement(context: Context) -> io::Result<HandleInp
                 email,
             } in context.table.row_iter()
             {
+                let (username_trimmed, _) = partition_point(username, |b| *b == 0);
+                let (email_trimmed, _) = partition_point(email, |b| *b == 0);
                 n += 1;
                 write!(context.output, "({}, ", id)?;
-                context.output.write(username)?;
+                context.output.write(username_trimmed)?;
                 context.output.write(b", ")?;
-                context.output.write(email)?;
+                context.output.write(email_trimmed)?;
                 context.output.write(b")\n")?;
             }
             writeln!(context.output, "{} rows.", n)?;
@@ -31,4 +33,18 @@ pub(in crate::cli) fn handle_statement(context: Context) -> io::Result<HandleInp
     }
 
     Ok(HandleInputOutcome::Continue)
+}
+
+// Not yet stable in Rust stdlib. This is a slice method in nightly Rust.
+fn partition_point<T, P>(slice: &[T], mut pred: P) -> (&[T], &[T])
+where
+    P: FnMut(&T) -> bool,
+{
+    for i in 0..slice.len() {
+        if pred(&slice[i]) {
+            return slice.split_at(i);
+        }
+    }
+
+    slice.split_at(slice.len())
 }
